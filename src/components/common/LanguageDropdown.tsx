00b40a1e8d62852110e3a50cce9062518de7af29
MEK <mohamedemadkamal1@gmail.com>
2026-08-23 17:49:57 +0300

    Fixes the iOS language issue

--- src/components/common/LanguageDropdown.tsx
+++ src/components/common/LanguageDropdown.tsx
@@ -158,7 +158,6 @@ export function LanguageDropdown({
       <Modal
         animationType="fade"
         navigationBarTranslucent
-        onOrientationChange={close}
         onRequestClose={close}
         presentationStyle="overFullScreen"
         statusBarTranslucent
